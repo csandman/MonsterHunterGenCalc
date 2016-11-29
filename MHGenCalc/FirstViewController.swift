bc9d77e54ed49951ad495a77280112e447c62d4b
Student <student@ip0af50f6b.int.uvm.edu>
2016-11-29 15:05:56 -0500

    Added CoreData??? Maybe

--- MHGenCalc/FirstViewController.swift
+++ MHGenCalc/FirstViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import CoreData
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource {
     
     
     @IBOutlet weak var humanPalico: UISegmentedControl!
@@ -28,9 +29,8 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var progressLabel: UILabel!
     
    
-    
-    
-
+    var displayStrings = [String]()
+    //var people = [Person]()
     @IBAction func indexChange(_ sender: Any) {
         //switch segmentedControl.selectedSegmentIndex
         //{
@@ -45,14 +45,80 @@ class FirstViewController: UIViewController {
         //}
         
     }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         savedLabel.textAlignment = NSTextAlignment.center;
         progressLabel.textAlignment = NSTextAlignment.center;
+        
+        //savedTable.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        //progressTable.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        //let appDelegate =
+            //UIApplication.shared.delegate as! AppDelegate
+        
+        //let managedContext = appDelegate.managedObjectContext
+        
+        //let entity =  NSEntityDescription.entity(forEntityName: "Person", in:managedContext)
+        //let person = Person(entity: entity!, insertInto: managedContext)
+        
+        //person.name = "aaaaaa"
+        
+        //do {
+        //    try managedContext.save()
+       //     people.append(person)
+        //} catch let error as NSError  {
+       //     print("Could not save \(error), \(error.userInfo)")
+       // }
+        
+        //let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Person")
+        
+       // do {
+        //    let results =
+        //        try managedContext.fetch(fetchRequest)
+         //   people = results as! [Person]
+            
+         //   for p in people
+         //   {
+         //       people.append(p)
+         //       self.displayStrings.append(p.name!)
+         //       print(p.name!)
+          //  }
+            
+         //   self.savedTable.reloadData()
+            
+            
+                
+            
+       // } catch let error as NSError {
+        //    print("fetch or save failed \(error), \(error.userInfo)")
+      //  }
     }
 
+    
+    // MARK: UITableViewDataSource
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return displayStrings.count
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt
+        indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        
+        cell!.textLabel!.text = displayStrings[indexPath.row]
+        
+        return cell!
+    }
+    
+
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
