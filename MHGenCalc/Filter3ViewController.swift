3658d2b8704f843b64c35bebbf6bffe0aaf8aaed
Student <student@ip0af502d0.int.uvm.edu>
2016-12-13 01:09:02 -0500

    added filter 3 stuff

--- MHGenCalc/Filter3ViewController.swift
+++ MHGenCalc/Filter3ViewController.swift
@@ -12,8 +12,54 @@ class Filter3ViewController: UIViewController {
 
     @IBOutlet weak var hunterLabel: UILabel!
     
-    @IBOutlet weak var bladeMasterSwitch: UISwitch!
+    @IBOutlet weak var bladeSwitch: UISwitch!
     @IBOutlet weak var gunnerSwitch: UISwitch!
+    
+    
+    var bladeValue = 0;
+    var gunnerValue = 0;
+    
+    var arrayToPass3 = Array(repeating: 0, count: 2)
+
+    
+    @IBAction func bladeFlag(_ sender: Any) {
+        let appDelegate =
+        UIApplication.shared.delegate as! AppDelegate
+        if(bladeSwitch.isOn==true)
+        {
+            print("good")
+            bladeValue = 1
+            print(bladeValue)
+            arrayToPass3[0] = bladeValue
+        }
+        else{
+            print("bad")
+            bladeValue = 0
+            print(bladeValue)
+            arrayToPass3[0] = bladeValue
+        }
+        appDelegate.filterThreeArr = arrayToPass3
+        
+    }
+    @IBAction func gunnerFlag(_ sender: Any) {
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        if(gunnerSwitch.isOn==true)
+        {
+            print("good")
+            gunnerValue = 1
+            print(gunnerValue)
+            arrayToPass3[1] = gunnerValue
+        }
+        else{
+            print("bad")
+            gunnerValue = 0
+            print(gunnerValue)
+            arrayToPass3[1] = gunnerValue
+        }
+        appDelegate.filterThreeArr = arrayToPass3
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
