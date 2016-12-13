4e6b406d49980b760ebb91c131bde357b68473d2
Student <student@ip0af502d0.int.uvm.edu>
2016-12-12 23:14:11 -0500

    woops

--- MHGenCalc/Filter2ViewController.swift
+++ MHGenCalc/Filter2ViewController.swift
@@ -18,6 +18,112 @@ class Filter2ViewController: UIViewController {
     @IBOutlet weak var thunderSwitch: UISwitch!
     @IBOutlet weak var dragonSwitch: UISwitch!
     
+    var fireValue = 0;
+    var waterValue = 0;
+    var iceValue = 0;
+    var thunderValue = 0;
+    var dragonValue = 0;
+    
+    var arrayToPass2 = Array(repeating: 0, count: 5)
+    
+    @IBAction func fireFlag(_ sender: AnyObject) {
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        if(fireSwitch.isOn==true)
+        {
+            print("good")
+            fireValue = 1
+            print(fireValue)
+            arrayToPass2[0] = fireValue
+        }
+        else{
+            print("bad")
+            fireValue = 0
+            print(fireValue)
+            arrayToPass2[0] = fireValue
+        }
+        appDelegate.filterTwoArr = arrayToPass2
+    }
+    @IBAction func waterFlag(_ sender: AnyObject) {
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        if(waterSwitch.isOn==true)
+        {
+            print("good")
+            waterValue = 1
+            print(waterValue)
+            arrayToPass2[1] = waterValue
+        }
+        else{
+            print("bad")
+            waterValue = 0
+            print(waterValue)
+            arrayToPass2[1] = waterValue
+        }
+        appDelegate.filterTwoArr = arrayToPass2
+        
+    }
+    @IBAction func iceFlag(_ sender: AnyObject) {
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        if(iceSwitch.isOn==true)
+        {
+            print("good")
+            iceValue = 1
+            print(iceValue)
+            arrayToPass2[2] = iceValue
+            
+        }
+        else{
+            print("bad")
+            iceValue = 0
+            print(iceValue)
+            arrayToPass2[2] = iceValue
+        }
+        appDelegate.filterTwoArr = arrayToPass2
+        
+    }
+    @IBAction func thunderFlag(_ sender: AnyObject) {
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        if(thunderSwitch.isOn==true)
+        {
+            print("good")
+            thunderValue = 1
+            print(thunderValue)
+            arrayToPass2[3] = thunderValue
+        }
+        else{
+            print("bad")
+            thunderValue = 0
+            print(thunderValue)
+            arrayToPass2[3] = thunderValue
+        }
+        appDelegate.filterTwoArr = arrayToPass2
+        
+    }
+    @IBAction func dragonFlag(_ sender: AnyObject) {
+        print(arrayToPass2)
+        let appDelegate =
+            UIApplication.shared.delegate as! AppDelegate
+        if(dragonSwitch.isOn==true)
+        {
+            print("good")
+            dragonValue = 1
+            print(dragonValue)
+            arrayToPass2[4] = dragonValue
+        }
+        else{
+            print("bad")
+            dragonValue = 0
+            print(dragonValue)
+            arrayToPass2[4] = dragonValue
+        }
+        appDelegate.filterTwoArr = arrayToPass2
+        
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
